88222d80106524ade31ab6f541fe2bab67931e97
Andriy <pavliv.andriy92@gmail.com>
2017-04-21 21:22:33 +0300

    added tests for api controller and data service

---
+++ TestAssignment.Tests/ApiControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TestAssignment.Controllers.API;
+using TestAssignment.Infrastructure.EF;
+using TestAssignment.Models.EmployeeSales;
+using TestAssignment.Services;
+
+namespace TestAssignment.Tests
+{
+    [TestFixture]
+    public class ApiControllerTests
+    {
+        [Test]
+        public void HomeController_Search_Method_Test()
+        {
+            //Arrange
+            var dataSvcMock = new Mock<IDataService>();
+            var filter = Mock.Of<EmployeeSalesFilterModel>(f =>
+                        f.Name == string.Empty && 
+                        f.PageIdx == 1 && 
+                        f.PageSize == 5);
+
+            var employeeSales = new List<EmployeeSales>
+            {
+                new EmployeeSales
+                {
+                    EmployeeID = 1
+                }
+            };
+
+            dataSvcMock.Setup(t => t.LoadEmployeeSales(1,5, string.Empty))
+                                  .Returns(employeeSales);           
+
+            var homeController = new HomeController(dataSvcMock.Object);
+
+            //Act
+
+            var employeeSalesModel = homeController.Search(filter);
+
+            //Assert
+            Assert.That(employeeSalesModel, Is.Not.Null);
+            dataSvcMock.Verify(t => t.LoadEmployeeSales(1,5, string.Empty), Times.Once());
+        }
+    }
+}
---
+++ TestAssignment.Tests/DataServiceTests.cs
@@ -0,0 +1,38 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using TestAssignment.Infrastructure.EF;
+using TestAssignment.Services;
+
+namespace TestAssignment.Tests
+{
+    [TestFixture]
+    public class DataServiceTests
+    {
+        [Test]
+        public void DataService_LoadEmployeeSales_Test()
+        {
+            //Arrange
+            var dataSvcMock = new Mock<IDataService>();
+            
+            var employeeSales = new List<EmployeeSales>
+            {
+                new EmployeeSales
+                {
+                    EmployeeID = 1
+                }
+            };
+
+            dataSvcMock.Setup(t => t.LoadEmployeeSales(1, 5, string.Empty))
+                                  .Returns(employeeSales);
+
+            //Act
+            var actual = dataSvcMock.Object.LoadEmployeeSales(1, 5, string.Empty);            
+
+            //Assert
+            Assert.That(actual, Is.Not.Null);
+            Assert.That(actual.Count == 1);
+            dataSvcMock.Verify(t => t.LoadEmployeeSales(1, 5, string.Empty), Times.Once());
+        }        
+    }
+}
