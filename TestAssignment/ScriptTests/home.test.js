024b7d152aaeae59d6fcf8724ebfa99f9d7ce28d
Andriy <pavliv.andriy92@gmail.com>
2017-04-22 00:17:02 +0300

    added infrastructure for JS unit tests. Added some draft Jasmine test structure.

---
+++ TestAssignment/ScriptTests/home.test.js
@@ -0,0 +1,38 @@
+﻿describe('Home view model tests', function () {
+    var HomeViewModel = null;
+    var firstPageIndex = 1;	
+    
+    beforeEach(function () {
+        //TODO: init model
+    });
+
+    afterEach(function () {
+        
+    });
+
+    it("HomeViewModel exists", function () {
+        expect(HomeViewModel).not.toBe(undefined);
+        expect(HomeViewModel).not.toBe(null);
+    });
+
+    it("HomeViewModel filter exists", function () {
+        expect(HomeViewModel.Filter).not.toBe(undefined);
+        expect(HomeViewModel.Filter).not.toBe(null);
+    });
+
+    it("Can switch to first page", function () {
+        //arange
+        //act
+        HomeViewModel.FirstPage();
+        //assert
+        expect(HomeViewModel.Filter.PageIdx()).toBe(firstPageIndex);
+    });
+
+    it("Can clear filter", function () {
+        //arange
+        //act
+        HomeViewModel.ClearSearch();
+        //assert
+        expect(HomeViewModel.Filter.Name()).toBe("");        
+    });
+});
